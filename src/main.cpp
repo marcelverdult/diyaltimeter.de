76c440364df16bae24c94489c4ad62c33c70d8fb
Marcel Verdult <37555613+marcelverdult@users.noreply.github.com>
2019-05-03 22:52:34 +0200

    code v0.02

--- src/main.cpp
+++ src/main.cpp
@@ -7,10 +7,10 @@
 #include <U8g2lib.h>
 #include <SPI.h>
 
-#define MODE_GROUND 3
-#define MODE_AIRPLANE 2 
 #define MODE_FREEFALL 0
 #define MODE_CANOPY 1
+#define MODE_AIRPLANE 2
+#define MODE_GROUND 3
 #define MODE_MENU 4
 #define MODE_UPDATE 5
 
@@ -27,6 +27,8 @@
 #define PIN_DISPLAY_DC 14
 #define PIN_DISPLAY_RESET 12
 
+#define PIN_BATTERY 35
+
 #define uS_TO_S_FACTOR 1000000 //Conversion factor for micro seconds to seconds
 #define mS_TO_S_FACTOR 1000    //Conversion factor for milli seconds to seconds
 
@@ -42,21 +44,36 @@ Button buttonDown(PIN_BUTTON_DOWN);
 Button buttonEnter(PIN_BUTTON_ENTER);
 
 // define constants
-const unsigned long LONG_PRESS = 1000; // what counts as long press
-const unsigned long sleepTime = 30;    // after what time to sleep
-const unsigned long sleepForTime = 30; // sleep for how long
+const unsigned long LONG_PRESS = 1000;   // what counts as long press
+const unsigned long sleepTime = 30;      // after what time to sleep
+const unsigned long sleepForTime = 30;   // sleep for how long
+const int batteryCheckInterval = 300000; // in ms = every 5 min if not in freefall/canopy mode
+const int timeCheckInterval = 1000;
+const int displayUpdateInterval = 5000;
 
 // define variables
 bool debug = true; // debug mode? enables Serial Messages
 bool demo = false;
-byte mode;         // current work mode
+byte mode; // current work mode
 unsigned long currentMillis = 0;
 unsigned long groundTime = 0;
 int altitude1;
 int altitude2;
 int currentAltitude;
 
+RtcDateTime now;
+char currentDateTime[20];
+char currentTime[6];
+char currentDate[11];
+
+int vbat;
+float voltage; // battery voltage
+byte batteryLevel;
+
 unsigned long lastAltiCheck = 0;
+unsigned long lastBatteryCheck = 0;
+unsigned long lastTimeCheck = 0;
+unsigned long lastDisplayUpdate = 0;
 unsigned long lastAction = 0;
 
 // variables to keep during sleep
@@ -94,18 +111,94 @@ void readButtons()
 
 void checkAltitude()
 {
-  if(!demo)
+  if (!demo)
   {
-  if (lastAltiCheck + 200 < currentMillis)
-  {
-    altitude1 = pressureSensor1.readAltitude(defaultPressure1);
-    //altitude2 = pressureSensor2.readAltitude(defaultPressure2);
-    //currentAltitude = ( altitude1 + altitude2 ) / 2;
-    currentAltitude = altitude1;
-    lastAltiCheck = currentMillis;
-  }
+    if (lastAltiCheck + 200 < currentMillis)
+    {
+      altitude1 = pressureSensor1.readAltitude(defaultPressure1);
+      //altitude2 = pressureSensor2.readAltitude(defaultPressure2);
+      //currentAltitude = ( altitude1 + altitude2 ) / 2;
+      currentAltitude = altitude1;
+      lastAltiCheck = currentMillis;
+    }
   }
 };
+
+/* -------------------------------------------------------------------------------------------------------- */
+
+void getTime()
+{
+  if (lastTimeCheck + timeCheckInterval < currentMillis || lastTimeCheck == 0)
+  {
+    now = rtc.GetDateTime();
+
+    snprintf_P(currentDateTime,
+               sizeof(currentDateTime),
+               PSTR("%02u.%02u.%04u %02u:%02u:%02u"),
+               now.Day(),
+               now.Month(),
+               now.Year(),
+               now.Hour(),
+               now.Minute(),
+               now.Second());
+
+    snprintf_P(currentDate,
+               sizeof(currentDate),
+               PSTR("%02u.%02u.%04u"),
+               now.Day(),
+               now.Month(),
+               now.Year());
+
+    snprintf_P(currentTime,
+               sizeof(currentTime),
+               PSTR("%02u:%02u"),
+               now.Hour(),
+               now.Minute());
+    lastTimeCheck = currentMillis;
+  }
+}
+
+/* -------------------------------------------------------------------------------------------------------- */
+
+void checkBattery()
+{
+  if (mode > 2)
+  {
+    if (lastBatteryCheck + batteryCheckInterval < currentMillis || lastBatteryCheck == 0)
+    {
+      vbat = analogRead(35);
+      voltage = 7.445 * vbat / 4096.0;
+      debugMessage("Battery voltage");
+      debugMessage((String)voltage);
+      lastBatteryCheck = currentMillis;
+
+      if (voltage > 4.0)
+      {
+        batteryLevel = 5;
+      }
+      else if (voltage > 3.9)
+      {
+        batteryLevel = 4;
+      }
+      else if (voltage > 3.8)
+      {
+        batteryLevel = 3;
+      }
+      else if (voltage > 3.7)
+      {
+        batteryLevel = 2;
+      }
+      else if (voltage > 3.6)
+      {
+        batteryLevel = 1;
+      }
+      else
+      {
+        batteryLevel = 0;
+      }
+    }
+  }
+}
 
 /* -------------------------------------------------------------------------------------------------------- */
 
@@ -118,6 +211,15 @@ void changeModeTo(byte newMode)
 };
 
 /* -------------------------------------------------------------------------------------------------------- */
+
+void displayBatteryLevel()
+{
+  // display battery level
+  u8g2.setFontDirection(1);
+  u8g2.setCursor(106, 0);
+  u8g2.setFont(u8g2_font_battery19_tn);
+  u8g2.print(batteryLevel);
+}
 
 // check if altitude changed during sleep
 // yes -> airplaneMode
@@ -137,7 +239,7 @@ void checkAltitudeAfterWakeup()
   if (currentAltitude > altitudeBeforeSleep + 50)
   {
     debugMessage("Altitude change! Switching to planeMode");
-    changeModeTo(2); // change to planeMode
+    changeModeTo(MODE_AIRPLANE);
   }
   else
   {
@@ -164,7 +266,30 @@ void groundMode()
   if (buttonEnter.wasPressed())
   {
     groundTime = 0;
-    changeModeTo(4); // change Mode to menuMode
+    changeModeTo(MODE_MENU); // change Mode to menuMode
+  }
+
+  if (lastDisplayUpdate + displayUpdateInterval < currentMillis || lastDisplayUpdate == 0)
+  { // display
+    u8g2.clearBuffer();
+    u8g2.setFontDirection(0);
+    u8g2.setFont(u8g2_font_courR08_tf);
+    u8g2.setCursor(0, 8);
+    u8g2.print("8==========D");
+    u8g2.drawHLine(0, 10, 128);
+
+    u8g2.setFont(u8g2_font_courB24_tn);
+    u8g2.setCursor(0, 34);
+    u8g2.print(currentTime);
+
+    u8g2.setFont(u8g2_font_courB10_tn);
+    u8g2.setCursor(0, 46);
+    u8g2.print(currentDate);
+
+    displayBatteryLevel();
+
+    u8g2.sendBuffer();
+    lastDisplayUpdate = currentMillis;
   }
 };
 
@@ -184,35 +309,37 @@ void airplaneMode()
   else if (lastAction + 20000 < currentMillis)
   {
     debugMessage("DEMO: arrived at 4000m exit no -> freefallMode");
-    changeModeTo(0); // change to freefallMode
+    changeModeTo(MODE_FREEFALL);
   }
 };
 
 /* -------------------------------------------------------------------------------------------------------- */
 
-void freefallMode(){
- if (lastAction == 0)
+void freefallMode()
+{
+  if (lastAction == 0)
   {
     lastAction = currentMillis;
   }
   else if (lastAction + 20000 < currentMillis)
   {
     debugMessage("DEMO: freefall ended -> canopyMode");
-    changeModeTo(1); // change to freefallMode
+    changeModeTo(MODE_CANOPY);
   }
 };
 
 /* -------------------------------------------------------------------------------------------------------- */
 
-void canopyMode(){
-   if (lastAction == 0)
+void canopyMode()
+{
+  if (lastAction == 0)
   {
     lastAction = currentMillis;
   }
   else if (lastAction + 20000 < currentMillis)
   {
     debugMessage("DEMO: landed! -> groundMode");
-    changeModeTo(3); // change to groundMode
+    changeModeTo(MODE_GROUND);
   }
 };
 
@@ -222,9 +349,7 @@ void menuMode()
 {
   if (buttonEnter.pressedFor(2000))
   {
-    debugMessage("switching to groundMode");
-    mode = 3;
-    delay(500);
+    changeModeTo(MODE_GROUND);
   }
 };
 
@@ -239,8 +364,9 @@ void updateMode(){
 void setup()
 {
   if (debug)
+  {
     Serial.begin(115200);
-
+  }
   pressureSensor1.begin(0x76);
   // pressureSensor2.begin(0x77);
 
@@ -275,7 +401,7 @@ void setup()
     u8g2.enableUTF8Print();
     u8g2.setFlipMode(1);
 
-    mode = 3;
+    mode = MODE_GROUND;
   }
 }
 
@@ -286,6 +412,8 @@ void loop()
   currentMillis = millis();
   checkAltitude();
   readButtons();
+  getTime();
+  checkBattery();
 
   switch (mode)
   {
